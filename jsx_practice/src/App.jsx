2f6d0d51af73044c70b3e54025c9cf119c4013e8
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-05-27 22:36:19 +0300

    jsx rules

--- jsx_practice/src/App.jsx
+++ jsx_practice/src/App.jsx
@@ -1,5 +1,16 @@
+import JSXRules from "./components/JSXRules";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 export default function App() {
-  return <WelcomeMessage />;
+  return (
+    <div>
+      <WelcomeMessage />
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input type="text" placeholder="Enter your name" />
+      </form>
+
+      <JSXRules />
+    </div>
+  );
 }
---
+++ jsx_practice/src/components/JSXRules.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p>
+        JSX must return a single parent Element <br />
+        JSX elements must be properly closed <br />
+        JSX attributes are written using camelCase"(e.g., className instead of
+        class)"
+      </p>
+    </div>
+  );
+};
+
+export default JSXRules;
