ccbba33703c286356cd0027a009abd74e0db0cd1
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-05-28 22:26:51 +0300

    used destructuring to iterate through the user's information data - {usersInfo.map(({ username, email, location }) =>{        <ul key={Math.random()}>           <li>{username}</li>           <li>{email}</li>           <li>{location}</li>         </ul> )) }

--- lists_map/src/App.jsx
+++ lists_map/src/App.jsx
@@ -2,13 +2,25 @@ import usersInfo from "./userInfo";
 export default function App() {
   return (
     <main>
-      {usersInfo.map((user) => (
+      {usersInfo.map(({ username, email, location }) => (
         <ul key={Math.random()}>
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>{user.location}</li>
+          <li>{username}</li>
+          <li>{email}</li>
+          <li>{location}</li>
         </ul>
       ))}
     </main>
   );
+}
+
+{
+  /* <main>
+  {usersInfo.map((user) => (
+    <ul key={Math.random()}>
+      <li>{user.username}</li>
+      <li>{user.email}</li>
+      <li>{user.location}</li>
+    </ul>
+  ))}
+</main>; */
 }
