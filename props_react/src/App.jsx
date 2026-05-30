ad4c4bd5d8328acb1cab6a2fdd278e550e280c0c
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-05-30 23:30:50 +0300

    Props destructuring

--- props_react/src/App.jsx
+++ props_react/src/App.jsx
@@ -12,17 +12,30 @@ const App = () => {
   );
 };
 
-const User = (props) => {
-  console.log(props);
+// Props destructuring
+const User = ({ img, name, age, isMarried, hobbies }) => {
   return (
     <section>
-      <img src={props.img} alt={props.name} />
-      <h1>Name: {props.name}</h1>
-      <h2>Age: {props.age}</h2>
-      <h3>Is married: {props.isMarried}</h3>
-      <h4>Hobbies: {props.hobbies}</h4>
+      <img src={img} alt={name} />
+      <h1>Name: {name}</h1>
+      <h2>Age: {age}</h2>
+      <h3>Is married: {isMarried}</h3>
+      <h4>Hobbies: {hobbies}</h4>
     </section>
   );
 };
+
+// const User = (props) => {
+//   console.log(props);
+//   return (
+//     <section>
+//       <img src={props.img} alt={props.name} />
+//       <h1>Name: {props.name}</h1>
+//       <h2>Age: {props.age}</h2>
+//       <h3>Is married: {props.isMarried}</h3>
+//       <h4>Hobbies: {props.hobbies}</h4>
+//     </section>
+//   );
+// };
 
 export default App;
