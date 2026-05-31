60e243f91e6ace047f684dd3bb00ef3dea81432b
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-05-31 21:33:53 +0300

    used props to access children i.e props.children

---
+++ card_project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Card>
+        <h1>My Card</h1>
+        <p>This is some content for card 1</p>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
