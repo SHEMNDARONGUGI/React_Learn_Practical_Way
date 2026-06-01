ba4647650064bdced11ebd32a72e13acc5facc31
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-06-01 14:08:03 +0300

    conditional rendering using ternary operator

--- conditional_rendering/src/App.jsx
+++ conditional_rendering/src/App.jsx
@@ -2,11 +2,12 @@ const ValidPassword = () => <h1>Valid Password</h1>;
 const InvalidPassword = () => <h1>Invalid Password</h1>;
 
 const Password = ({ isValid }) => {
-  if (isValid) {
-    return <ValidPassword />;
-  }
+  // if (isValid) {
+  //   return <ValidPassword />;
+  // }
 
-  return <InvalidPassword />;
+  // return <InvalidPassword />;
+  return isValid ? <ValidPassword /> : <InvalidPassword />;
 };
 const App = () => {
   return <Password isValid={true} />;
