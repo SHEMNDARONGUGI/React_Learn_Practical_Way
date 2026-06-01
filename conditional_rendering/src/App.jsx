2909a752e837e19cc024b564847be1f825bf77af
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-06-01 14:02:51 +0300

    Conditional rendering

---
+++ conditional_rendering/src/App.jsx
@@ -0,0 +1,15 @@
+const ValidPassword = () => <h1>Valid Password</h1>;
+const InvalidPassword = () => <h1>Invalid Password</h1>;
+
+const Password = ({ isValid }) => {
+  if (isValid) {
+    return <ValidPassword />;
+  }
+
+  return <InvalidPassword />;
+};
+const App = () => {
+  return <Password isValid={true} />;
+};
+
+export default App;
