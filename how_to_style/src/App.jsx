5d3f1d1a24ee89acd70abc81a6789f9d88067e52
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-06-08 22:12:52 +0300

    Adding style to index.css

--- how_to_style/src/App.jsx
+++ how_to_style/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const App = () => {
-  const styles = {
-    color: "red",
-    backgroundColor: "teal",
-    padding: "2rem",
-  };
+  // const styles = {
+  //   color: "red",
+  //   backgroundColor: "teal",
+  //   padding: "2rem",
+  // };
   return (
     <section>
       <h1 style={styles}>Inline Style</h1>
