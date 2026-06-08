0dbbeab0e4fdb6b09b70756f99a1e4fc22d3a0f0
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-06-08 21:59:43 +0300

    Styling xml element - use double curly braces{{ }} -normal css attributes using (-) are written using camel casing as 1 word where the beginning of the second starts with a capital letter i.e. background-color is written as backgroundColor

--- how_to_style/src/App.jsx
+++ how_to_style/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <section>
+      <h1 style={{ color: "red", backgroundColor: "teal", padding: "2rem" }}>
+        Inline Style
+      </h1>
+    </section>
+  );
 };
 
 export default App;
