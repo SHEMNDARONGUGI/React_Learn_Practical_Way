e5192b2c1d892b6484e2fe9b0b9f4245a70fa629
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-06-19 15:29:59 +0300

    How to use... useState to update objects

--- stateHooks/src/App.jsx
+++ stateHooks/src/App.jsx
@@ -1,29 +1,47 @@
 import { useState } from "react";
 
 const App = () => {
+  const [movie, setMovie] = useState({
+    title: "Chinese Zodiac",
+    ratings: 7,
+  });
+
+  const handleClick = () => {
+    // const copyMovie = {
+    //   ...movie,
+    //   ratings: 5,
+    // };
+
+    setMovie({ ...movie, ratings: 5 });
+  };
   // const [count, setCount] = useState(0);
   // console.log(count);
   // const increment = () => setCount(count + 1);
   // const decrement = () => setCount(count - 1);
-  const [friends, setFriends] = useState(["Alex", "John"]);
+  // const [friends, setFriends] = useState(["Alex", "John"]);
 
-  function addOneFriend() {
-    setFriends([...friends, "Shem Ndaro"]);
-  }
+  // function addOneFriend() {
+  //   setFriends([...friends, "Shem Ndaro"]);
+  // }
 
-  const removeFriend = () => setFriends(friends.filter((f) => f !== "John"));
+  // const removeFriend = () => setFriends(friends.filter((f) => f !== "John"));
 
-  const updateOneFriend = () => {
-    setFriends(friends.map((f) => (f === "Alex" ? "Alex Smith" : f)));
-  };
+  // const updateOneFriend = () => {
+  //   setFriends(friends.map((f) => (f === "Alex" ? "Alex Smith" : f)));
+  // };
 
   return (
     <section>
+      <h1>Title: {movie.title}</h1>
+      <p>ratings: {movie.ratings}</p>
+
+      <button onClick={handleClick}>Change Rating</button>
+
       {/* <h1>{count}</h1>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button> */}
 
-      <ul>
+      {/* <ul>
         {friends.map((f, id) => (
           <li key={id}>{f}</li>
         ))}
@@ -31,7 +49,7 @@ const App = () => {
 
       <button onClick={addOneFriend}>Add New Friend</button>
       <button onClick={removeFriend}>Remove Friend</button>
-      <button onClick={updateOneFriend}>Update One Friend</button>
+      <button onClick={updateOneFriend}>Update One Friend</button> */}
     </section>
   );
 };
