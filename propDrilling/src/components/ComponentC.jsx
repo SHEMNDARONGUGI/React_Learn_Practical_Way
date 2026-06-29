1a03a7c995d51f3170b0fe6e0e483a17c0c223cd
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-06-29 16:17:47 +0300

    useContext => How to use useContext to get prop details from parent component instead of using contextAPI(consumer) callbacks

--- propDrilling/src/components/ComponentC.jsx
+++ propDrilling/src/components/ComponentC.jsx
@@ -1,24 +1,13 @@
+import { useContext } from "react";
 import { Data, Data1 } from "../App";
 
 function ComponentC() {
+  const userName = useContext(Data);
+  const age = useContext(Data1);
   return (
-    <Data.Consumer>
-      {(name) => {
-        // return <h1>{name}</h1>;
-
-        return (
-          <Data1.Consumer>
-            {(age) => {
-              return (
-                <h1>
-                  My name is {name} and I am {age} years old.
-                </h1>
-              );
-            }}
-          </Data1.Consumer>
-        );
-      }}
-    </Data.Consumer>
+    <h1>
+      My name is: {userName} and I'm {age} years old.
+    </h1>
   );
 }
 
