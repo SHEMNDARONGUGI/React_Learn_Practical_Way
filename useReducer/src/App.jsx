8d6ae1cf3eb705dd653da2936d35ddb1f0d0a6e7
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-02 18:37:34 +0300

    useReducer Exercise => making a complex counter

--- useReducer/src/App.jsx
+++ useReducer/src/App.jsx
@@ -1,34 +1,46 @@
-import { useReducer } from "react";
-
-const initialState = { count: 0 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { ...state, count: state.count + 1 };
-      break;
-
-    case "decrement":
-      return { ...state, count: state.count - 1 };
-
-    case "reset":
-      return { ...state, count: 0 };
-
-    default:
-      break;
-  }
-};
+import Counter from "./components/Counter";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
-      <h1>Count: {state.count}</h1>
+      <Counter />
     </div>
   );
 };
 
 export default App;
+
+// import { useReducer } from "react";
+
+// const initialState = { count: 0 };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { ...state, count: state.count + 1 };
+//       break;
+
+//     case "decrement":
+//       return { ...state, count: state.count - 1 };
+
+//     case "reset":
+//       return { ...state, count: 0 };
+
+//     default:
+//       break;
+//   }
+// };
+
+// const App = () => {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     <div>
+//       <button onClick={() => dispatch({ type: "increment" })}>+</button>
+//       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+//       <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+//       <h1>Count: {state.count}</h1>
+//     </div>
+//   );
+// };
+
+// export default App;
