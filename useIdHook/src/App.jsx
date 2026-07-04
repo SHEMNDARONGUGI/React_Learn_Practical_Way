37812ab06f5fa5ce565568f1a7b8ad002dc4a9de
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-04 23:42:50 +0300

    useId Hook

---
+++ useIdHook/src/App.jsx
@@ -0,0 +1,20 @@
+import UniqueID from "./components/UniqueID";
+
+const App = () => {
+  return (
+    <div>
+      <UniqueID />
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+        consectetur eos itaque voluptatum, nisi sunt fuga illo? Voluptates
+        eveniet nostrum nulla molestias sunt porro quibusdam, et rem sed iusto
+        quam?
+      </p>
+
+      <UniqueID />
+    </div>
+  );
+};
+
+export default App;
