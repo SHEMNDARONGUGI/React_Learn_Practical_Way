b560379d1631ddf2f739ba929153215551ab2786
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-07 18:31:00 +0300

    ToggleBackgroundColor Project

---
+++ react_projects_for_beginners/ToggleBackgroundColor/src/App.jsx
@@ -0,0 +1,11 @@
+import ToggleBackgroundColor from "./Components/ToggleBackgroundColor";
+
+const App = () => {
+  return (
+    <div>
+      <ToggleBackgroundColor />
+    </div>
+  );
+};
+
+export default App;
