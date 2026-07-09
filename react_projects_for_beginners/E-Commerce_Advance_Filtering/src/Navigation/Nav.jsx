9a71eea428286b7e8655235a04428d68122f39ab
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-09 18:25:56 +0300

    Product section worked on with demo data before I refactor the code

--- react_projects_for_beginners/E-Commerce_Advance_Filtering/src/Navigation/Nav.jsx
+++ react_projects_for_beginners/E-Commerce_Advance_Filtering/src/Navigation/Nav.jsx
@@ -1,3 +1,5 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Nav.css";
 export default function Nav() {
   return (
@@ -11,7 +13,17 @@ export default function Nav() {
       </div>
 
       <div className="profile-container">
-        <a href="#"></a>
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
       </div>
     </nav>
   );
