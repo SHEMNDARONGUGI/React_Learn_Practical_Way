670d7f062757186c528e18fa65f769b1b45109f8
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-14 14:10:09 +0300

    Type annotation in typescript

--- typescript_learning/index.ts
+++ typescript_learning/index.ts
@@ -1,9 +1,20 @@
 type Person = {
-    name: string;
-}
+  name: string;
+};
 
 const person: Person = {
-    name: "Shem"
+  name: "Shem",
 };
 
 console.log(person.name);
+
+let myFullName: string = "Shem Ndaro Ngugi";
+myFullName = "Ndaro Ngugi Shem";
+console.log(myFullName);
+
+let myAge: number = 21;
+console.log(`my name is: ${myFullName}
+I am: ${myAge}`);
+
+let tsHard: boolean = false;
+console.log(tsHard);
