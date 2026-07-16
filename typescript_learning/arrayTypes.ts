372976883394c3028a18eabd1bb5e1107969d703
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-16 23:24:04 +0300

    Arrays in typescript

---
+++ typescript_learning/arrayTypes.ts
@@ -0,0 +1,17 @@
+const nums: number[] = [1, 2, 4, 6];
+const str: string[] = ["one", "two", "three"];
+
+console.log(nums, str);
+
+//Adding Items to an array
+const items: string[] = [];
+items.push("Shem", "Steve", "Caleb");
+console.log(items);
+
+const numList: number[] = [];
+numList.push(20);
+console.log(numList);
+
+// Making an ray in another way
+const itemsArray: Array<string> = ["Shem", "Caleb", "Stever"];
+console.log(itemsArray);
