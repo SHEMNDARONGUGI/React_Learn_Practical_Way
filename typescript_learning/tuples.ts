43246119e4fe349a2ec7c17d309fabb1de9ceb1c
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-20 18:00:06 +0300

    Tuple -  Tuple is a type that represents an array with a fixed number of elements, where each element can have a different type.
    - The order of the types in the tuple definition corresponds to the order ot the values in the actual array.
    
    - Tuples are similar to arrays, but they have a specific structure and can be use to model finite sequenes with known length.

---
+++ typescript_learning/tuples.ts
@@ -0,0 +1,9 @@
+let myTuple: [number, string] = [10, "Hello World"];
+
+console.log(myTuple);
+
+const products: (number | string)[] = ["item 1", 12];
+console.log(products);
+
+const games: [string, number, boolean] = ["Game1", 2, true];
+console.log(games);
