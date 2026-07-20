9f366b4a68fa3faa65b569266fc8fbe61f0ba3c2
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-20 18:29:20 +0300

    Enums - Enum is a way to define a set of named constants. - Enums allow you to define a collection of related values that can be used interchangeably in your code

---
+++ typescript_learning/enums.ts
@@ -0,0 +1,8 @@
+enum WeatherConditions {
+  Sunny = "sunny",
+  Cloudy = "cloudy",
+  Rainy = "rainy",
+}
+
+const currentWeather = WeatherConditions.Cloudy;
+console.log(`The current weather is ${currentWeather}`);
