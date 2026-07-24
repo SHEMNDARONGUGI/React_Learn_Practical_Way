c8e92fa5285e4935fdbfe90e47985c6384356505
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-24 23:34:58 +0300

    interfaceWithClasses

---
+++ typescript_learning/interfaceWithClasses.ts
@@ -0,0 +1,18 @@
+interface Vehicle {
+  start(): void;
+  stop(): void;
+}
+
+class Car implements Vehicle {
+  start(): void {
+    console.log("Car is starting...");
+  }
+
+  stop(): void {
+    console.log("Car has stopped!");
+  }
+}
+
+const myCar = new Car();
+myCar.start();
+myCar.stop();
