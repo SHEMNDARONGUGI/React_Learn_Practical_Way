588b048aa0faddc0a687461f6a2201e5eca1f7c5
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-07-30 08:58:27 +0300

    TypeScript with Axios
    Working with axios
    Got the data from
    
    https://jsonplaceholder.typicode.com/

--- typescript_learning/Axios/index.ts
+++ typescript_learning/Axios/index.ts
@@ -1,1 +1,31 @@
 import axios from "axios";
+import type { AxiosResponse } from "axios";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+const fetchData = async () => {
+  try {
+    const response: AxiosResponse<Todo> = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    console.log("Todo: ", response.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios Error", error.message);
+
+      if (error.response) {
+        console.log("Status: ", error.response.status);
+
+        console.log("Data: ", error.response.data);
+      }
+    } else if (error instanceof Error) {
+      console.error("Error: ", error.message);
+    }
+  }
+};
+
+fetchData();
