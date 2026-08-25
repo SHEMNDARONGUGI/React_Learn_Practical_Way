a94ab4803a4c5c7fea91533aa749602219232a0c
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-08-25 23:42:33 +0300

    React with typescript as I prepare to work on my frontend

---
+++ react_with_typescript/ts-demos/src/components/User.tsx
@@ -0,0 +1,21 @@
+// type UserShape = {
+//   name: string;
+//   age: number;
+//   isStudent: boolean;
+// };
+
+import type { ReactNode } from "react";
+
+interface UserShape {
+  children: ReactNode;
+}
+
+const User = ({ children }: UserShape) => {
+  return (
+    <div>
+      <h1>{children}</h1>
+    </div>
+  );
+};
+
+export default User;
