a8c0aff85b517705bb590a3e65162ce0d610581b
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-09-16 21:20:24 +0300

    how will be providing type to our useState

---
+++ react_with_typescript/useState_types/src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const incrementCount = () => setCount(count + 1);
+  const decrementCount = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
