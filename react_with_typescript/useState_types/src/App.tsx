13e5f0fe80497a441e1d49f9e9ab0fef21abd3bf
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-09-16 22:46:05 +0300

    working with useState using typescript

--- react_with_typescript/useState_types/src/App.tsx
+++ react_with_typescript/useState_types/src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
+import UserProfile from "./components/UserProfile";
+import TodoList from "./components/TodoList";
 
 const App = () => {
+  const [age, setAge] = useState<number>(0);
+
+  const newAge = () => setAge(age + 10);
   return (
     <div>
       <Counter />
+
+      <p>Age: {age}</p>
+      <button onClick={newAge}>Get Age</button>
+
+      <UserProfile />
+      <TodoList />
     </div>
   );
 };
