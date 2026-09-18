7d69e859b922640625172225920aedde0c3af827
SHEMNDARONGUGI <ndaroshem@gmail.com>
2026-09-18 21:25:35 +0300

    useContext with typescript

---
+++ react_with_typescript/contextAPI/src/component/Counter.tsx
@@ -0,0 +1,14 @@
+import { useContext, type FC } from "react";
+import { MyContext } from "../context/MyContext";
+const Counter: FC = () => {
+  const { count, increment, decrement } = useContext(MyContext);
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
